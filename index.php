387ba2b346d6c7a1c73a185ae0f41288439cb381
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2024-11-19 01:58:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,27 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Details</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+    <title>Add Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php?>
-    <center>
-        <h1>Car Details</h1>
-        <br>
-    <div class = "col-4">
-    <form>
-        <input class="form-control" name="Car name" placeholder="Car Name">
-        <br>
-        <input class="form-control" name="Model" placeholder="Model">
-        <br>
-        <input class="form-control" name="price" placeholder="Price">
-        <br>
-        <button type="button" class="btn btn-primary">Add Book</button>
-    </form>
+    <div class="container mt-5">
+        <h2>Add New Product</h2>
+        <form action="save_product.php" method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label">Product Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" class="form-control" id="price" name="price" step="0.01" required>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Add Product</button>
+        </form>
+        <a href="product.php" class="btn btn-secondary mt-3">View All Products</a>
     </div>
-    </center>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
